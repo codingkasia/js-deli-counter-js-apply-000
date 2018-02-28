0aa76c2ff8a206c88eb607961d50adc55f6fb9c5
KATARZYNA ROSENBERG <codingkasia@gmail.com>
2018-02-28 02:16:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,10 +17,14 @@ function nowServing() {
 }
 
 function currentLine() {
-  if(katzDeliLine.length === 0){
+  var counter = 0;
+  if(katzDeli.length === 0){
     return "The line is currently empty."
   } else {
-    while(katzDeli)
+    while(katzDeli.length > counter) {
+      counter++;
+      
+    }
     ("The line is currently: 1. Bill, 2. Jane, 3. Ann");
   }
 }
