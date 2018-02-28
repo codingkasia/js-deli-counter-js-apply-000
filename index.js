e17f1ac1756779bd007a8456860b055232055c92
KATARZYNA ROSENBERG <codingkasia@gmail.com>
2018-02-28 01:45:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ var katzDeli = [];
 function takeANumber(katzDeliLine, newName) {
   var num = katzDeliLine.length;
   katzDeliLine.push(newName);
-  return `Welcome, Ada. You are number ${num} in line.`;
+  return `Welcome, ${newName}. You are number ${num} in line.`;
 }
 
