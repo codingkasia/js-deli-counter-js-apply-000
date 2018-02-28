0420a6f26cc66ffa016de83ff9b2b8b5aeda584b
KATARZYNA ROSENBERG <codingkasia@gmail.com>
2018-02-28 01:53:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,5 +7,21 @@ function takeANumber(katzDeliLine, newName) {
 }
 
 function nowServing() {
-  
+  if(katzDeli.length === 0) {
+    return `There is nobody waiting to be served!`;
+  } else {
+      return `Currently serving ${katzDeli[0]}.`
+    }
+    return "Booboo";
 }
+describe('nowServing', () => {
+    it('returns the line is empty when no one is on line', () => {
+      expect(nowServing([])).toEqual("There is nobody waiting to be served!");
+    });
+
+    it('returns an announcement about the person it is serving, and shifts the line', () => {
+      const deliLine = ["Steven", "Blake", "Avi"]
+      expect(nowServing(deliLine)).toEqual("Currently serving Steven.");
+      expect(deliLine).toEqual(["Blake", "Avi"]);
+    });
+  });
