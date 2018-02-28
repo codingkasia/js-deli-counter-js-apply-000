7f609acd0df37c520bdae7a672c0c9fd2a51dc18
KATARZYNA ROSENBERG <codingkasia@gmail.com>
2018-02-28 02:38:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function currentLine(katzDeli) {
     return "The line is currently empty."
   } 
     while(katzDeli.length > counter) {
-      tempArr.push(" " + counter+1 + ". " + katzDeli[counter]);
+      tempArr.push(" " + (counter+1) + ". " + katzDeli[counter]);
       counter++;
       }
       return `The line is currently:${tempArr}`;
